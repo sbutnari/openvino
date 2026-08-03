343df316ae964acb1b8f6e47734d786bfbb7d304
Razvan Apetroaie <razvan-mihai.apetroaie@intel.com>
2026-08-03 14:09:24 +0000

    windows measurements constant folding

--- src/core/src/pass/constant_folding.cpp
+++ src/core/src/pass/constant_folding.cpp
@@ -18,6 +18,21 @@
 #include "transformations/rt_info/decompression.hpp"
 #include "transformations/rt_info/dequantization_node.hpp"
 
+namespace {
+// clang-format off
+#include "windows.h"
+#include "psapi.h"
+// clang-format on
+
+void printMemoryValue() {  // Note: this value is in KB!
+    PROCESS_MEMORY_COUNTERS_EX2 pmc;
+    GetProcessMemoryInfo(GetCurrentProcess(), (PROCESS_MEMORY_COUNTERS*)&pmc, sizeof(pmc));
+    std::cout << "PrivateUsage: " << pmc.PrivateUsage / 1024 << " WorkingSetSize: " << pmc.WorkingSetSize / 1024
+              << " PrivateWorkingSetSize: " << pmc.PrivateWorkingSetSize / 1024 << "KB" << std::endl;
+}
+
+}  // namespace
+
 /**
  * \brief Check if \ref ov::Output<ov::Node> can be folded base on `can_be_folded` attribute.
  *
